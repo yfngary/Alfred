81086df45f114c6febb98d8d97bc4b58bcbf78a5
Kaleb Lundquist <kaleblundquist@mac.lan>
2025-02-01 11:14:02 -0700

    refactor: base app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Make sure App.js exists
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
